0296d223d5c61432ef89f61c800b0be8dec09bc3
Your Name <buzinay@mail.ru>
2017-05-11 20:13:35 +0400

    Исправлен скрипт

--- js/formshow.js
+++ js/formshow.js
@@ -5,21 +5,8 @@ var datein = form.querySelector("[name=date-in]");
 var dateout = form.querySelector("[name=date-out]");
 var adults = form.querySelector("[name=adults]");
 var children = form.querySelector("[name=children]");
-var storageadults = localStorage.getItem("adults");
-var storagechildren = localStorage.getItem("children");
 
-if (storageadults) {
-      adults.value = storageadults;
-  }
-
-if (storagechildren) {
-      children.value = storagechildren;
-  }
-
-if (formwrap.classList.contains("search-form-show")) {
-    formwrap.classList.toggle("search-form-hidden");
-    formwrap.classList.toggle("search-form-show");
-  }
+formwrap.classList.toggle("search-form-hidden");
 
 link.addEventListener("click", function(event) {
     event.preventDefault();
@@ -27,7 +14,6 @@ link.addEventListener("click", function(event) {
         formwrap.classList.remove("search-form-error");
       }
     formwrap.classList.toggle("search-form-hidden");
-    formwrap.classList.toggle("search-form-show");
     datein.focus();
   });
 
@@ -40,17 +26,12 @@ form.addEventListener("submit", function(event) {
       event.preventDefault();
       formwrap.classList.add("search-form-error");
     }
-    else {
-      localStorage.setItem("adults", adults.value);
-      localStorage.setItem("children", children.value);
-  }
-});
+  });
 
 window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
-      if (formwrap.classList.contains("search-form-show")) {
+      if (!formwrap.classList.contains("search-form-hidden")) {
         formwrap.classList.toggle("search-form-hidden");
-        formwrap.classList.toggle("search-form-show");
       }
       if (formwrap.classList.contains("search-form-error")){
         formwrap.classList.remove("search-form-error");
